b29c28a559a1c0c63a0bfeea4f58fb1f05a08ad2
Yona-Dav <yona.bohbot@gmail.com>
2021-11-23 12:08:26 +0200

    add family app

--- animals/models.py
+++ animals/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class Animal(models.Model):
-    name=models.CharField(max_length=200)
-    legs=models.PositiveIntegerField()
-    height=models.PositiveIntegerField()
-    speed=models.PositiveIntegerField()
+    name= models.CharField(max_length=200)
+    legs= models.PositiveIntegerField()
+    height= models.PositiveIntegerField()
+    speed= models.PositiveIntegerField()
+    family = models.ForeignKey('family.Family', on_delete=models.CASCADE)
